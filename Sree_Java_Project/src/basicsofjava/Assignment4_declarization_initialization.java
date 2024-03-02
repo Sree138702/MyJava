4c0c541113047f82f1dee987aea394f5277c6258
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-02 20:02:42 +0100

    Fourth Assignment

---
+++ Sree_Java_Project/src/basicsofjava/Assignment4_declarization_initialization.java
@@ -0,0 +1,21 @@
+package basicsofjava;
+
+public class Assignment4_declarization_initialization {
+	
+public static void main(String[]args) {
+	byte a=10;
+	short b=32123;
+	int c=98765;
+	long d=908765;
+	float e= 5.43f;
+	double a1=876.432;
+	char b1='S';
+	boolean c1=true;
+	boolean c2=false;
+	String name="Sree";
+	
+	
+	
+}
+
+}
