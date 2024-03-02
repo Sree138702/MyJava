55dd94d5a8a8da4b7b98083b8e3fff7e6f81143d
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-02 19:54:08 +0100

    Third Assignment

---
+++ Sree_Java_Project/src/basicsofjava/Assignment3_Calling_Staticmethod.java
@@ -0,0 +1,18 @@
+package basicsofjava;
+
+public class Assignment3_Calling_Staticmethod {
+	static void problem() {
+	int a=5000;
+	int b=17000;
+	int c=25;
+	int d=b-a+c;
+	System.out.println(d);
+
+	}
+	
+	public static void main (String[]args) {
+		problem();
+	
+	}
+	
+}
