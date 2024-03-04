b766ebb429497efa42bc1f93d4f3e7d3d8d4b77f
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-04 23:41:26 +0100

    Assignment2

---
+++ Sree_Java_Project/src/basicsofjava/Assignment2_linebyline_execution.java
@@ -0,0 +1,12 @@
+package basicsofjava;
+
+public class Assignment2_linebyline_execution {
+
+	public static void main (String[]args)
+	{
+System.out.println("My name is sree");
+System.out.println("learning automation");
+System.out.println("in grotechminds");
+System.out.println("want to become a software test engineer");
+
+}}
