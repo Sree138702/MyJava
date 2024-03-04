816a9cf0f1890aa1013e40cb311344673778d8d8
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-04 23:13:07 +0100

    Assignment 5

---
+++ Sree_Java_Project/src/basicsofjava/Assignment5.java
@@ -0,0 +1,15 @@
+package basicsofjava;
+
+public class Assignment5 {
+	public static void main(String[] args) {
+		
+	
+	int number1=2000;
+	System.out.println(number1);
+	number1=8000;
+	System.out.println(number1);
+	
+	
+}
+}
+
