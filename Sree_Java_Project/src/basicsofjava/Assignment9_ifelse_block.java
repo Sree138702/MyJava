d428e9314b9afd12be65a55efec10cb405a160c7
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-04 23:39:58 +0100

    Assignment9

---
+++ Sree_Java_Project/src/basicsofjava/Assignment9_ifelse_block.java
@@ -0,0 +1,20 @@
+package basicsofjava;
+
+public class Assignment9_ifelse_block {
+	public static void main (String[]args) {
+		
+		int age=12;
+		if (age<12) {
+			System.out.println("person is eligible to play football");
+		}
+		
+		else {
+			System.out.println("person is not eligible to play football");
+		}
+		
+		
+		
+	}
+	
+
+}
