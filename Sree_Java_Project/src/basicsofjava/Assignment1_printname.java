dab248b8d7957e319dbdb99a458953b632222e14
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-04 23:37:51 +0100

    Assignment6

---
+++ Sree_Java_Project/src/basicsofjava/Assignment1_printname.java
@@ -0,0 +1,14 @@
+package basicsofjava;
+
+public class Assignment1_printname {
+	
+	
+
+	public static void main (String[]args)
+	{
+		System.out.println("hello sree");
+		System.out.println("you will get the job soon");
+		System.out.println("as a software test engineer");
+}
+
+}
---
+++ Sree_Java_Project/src/basicsofjava/Assignment6.java
@@ -0,0 +1,14 @@
+package basicsofjava;
+
+public class Assignment6 {
+	static void add() {
+	int a=6700;
+	int b=1940;
+	int sum=a+b;
+	System.out.println(sum);}
+	
+	public static void main (String[]args) {
+	add();
+	
+
+}}
