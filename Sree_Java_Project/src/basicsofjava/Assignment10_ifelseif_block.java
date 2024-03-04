32fa48665c1e50802d4967a4fcc50f5469df587f
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-04 23:43:29 +0100

    Assignment 10

---
+++ Sree_Java_Project/src/basicsofjava/Assignment10_ifelseif_block.java
@@ -0,0 +1,22 @@
+package basicsofjava;
+
+public class Assignment10_ifelseif_block {
+	public static void main(String[] args) {
+		int a=30050;
+		int b=74000;{
+		
+		if (b!=a)//true//
+		{ System.out.println('A');
+		
+			
+		}
+		else if(a>b) {
+			System.out.println('B');
+		}
+		else;
+		System.out.println('c');}
+
+		
+	
+
+}}
