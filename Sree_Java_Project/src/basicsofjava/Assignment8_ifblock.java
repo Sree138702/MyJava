71a7fd0b4c10fe221db82ff2f33f0596c224dc3d
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-04 23:30:18 +0100

    Assignment8

---
+++ Sree_Java_Project/src/basicsofjava/Assignment8_ifblock.java
@@ -0,0 +1,23 @@
+package basicsofjava;
+
+public class Assignment8_ifblock {
+
+	public static void main (String[]args) {
+		int a=378;
+		int b=856;
+		{
+		if (a<b);
+		int sub=b-a;
+		System.out.println(sub);}
+		
+		{if (a==b);
+		
+			int sum=a=b;
+			System.out.println(sum);}
+					
+	}}
+	
+				
+	
+
+
