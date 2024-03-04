21bdc64fab87e876b1a19a50f423d36efe3aeb33
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-04 23:53:20 +0100

    Assignment 12

---
+++ Sree_Java_Project/src/basicsofjava/Assignment12_Or_operator.java
@@ -0,0 +1,26 @@
+package basicsofjava;
+
+public class Assignment12_Or_operator{
+	public static void main(String[] args) {
+		 
+		 char gender = 'M';
+		int age=20;
+		if(age>18|| gender=='M')
+		{
+			System.out.println("person is eligible to vote");
+			
+		}
+		
+		else 
+		{
+			
+			System.out.println("person is not eligible to vote");
+			
+		}
+		}
+		
+		
+
+	}
+	
+
