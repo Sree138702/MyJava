08d216af73b6ea1dfa594276ab3e228238ac196e
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-04 23:28:28 +0100

    Assignment7

---
+++ Sree_Java_Project/src/basicsofjava/Assignments7.java
@@ -0,0 +1,45 @@
+package basicsofjava;
+
+public class Assignments7{
+static void add() {
+	int a=1500;
+	int b=2800;
+	int c= a+b;
+	System.out.println(c);}
+	
+	static void sub() {
+		int a=1500;
+		int b=2800;
+		int c=b-a;
+		System.out.println(c);}
+		
+		static void mul() {
+		int a=1500;
+		int b=2800;
+		int c= a*b;
+		System.out.println(c);}
+		
+		static void div() {
+			int a=1500;
+			int b=2800;
+			int c=b/a;
+			System.out.println(c);}
+		static void mod() {
+			int a=1500;
+			int b=2800;
+			int c=b%a;
+			System.out.println(c);
+		}
+					
+		
+		
+		public static void main (String[]args) {
+		add();
+		sub();
+		mul();
+		div();
+		mod();
+	}}
+
+
+
