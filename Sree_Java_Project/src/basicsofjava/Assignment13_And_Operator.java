b0df3e68f787937456d7f2476cb0f87fd12fb8f0
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-05 11:15:11 +0100

    Assignment 13

---
+++ Sree_Java_Project/src/basicsofjava/Assignment13_And_Operator.java
@@ -0,0 +1,23 @@
+package basicsofjava;
+
+public class Assignment13_And_Operator {
+	public static void main(String[] args) {
+		
+		int age=25;
+		int salary=50000;
+		
+		if(age>30 && salary== 60000)
+		{
+			System.out.println("person can buy a car");	
+		}
+		
+		
+		else 
+		{
+			System.out.println("person can not buy a car");
+		}
+		}
+		
+	}
+
+
