41021e8fbac7c0acf943309a8b3161ebaea753b8
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-05 20:47:05 +0100

    Assignment 22.2

---
+++ Sree_Java_Project/src/basicsofjava/Area_Of_Rectangle.java
@@ -0,0 +1,14 @@
+package basicsofjava;
+//Assignment 22.2
+public class Area_Of_Rectangle {
+static int w=28;
+static int l=32;
+
+public static void main(String[] args) {
+	int Area_of_rectangle=w*l;
+	System.out.println(Area_of_rectangle);
+	
+	int Circumference_of_rectangle=2*w+2*l;
+	System.out.println(Circumference_of_rectangle);
+}
+}
