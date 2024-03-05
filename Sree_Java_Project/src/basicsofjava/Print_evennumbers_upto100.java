6a184a8d7cba8bfc3ed0a1eae9b0343544483e3b
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-05 22:57:32 +0100

    Assignment 25.1

---
+++ Sree_Java_Project/src/basicsofjava/Print_evennumbers_upto100.java
@@ -0,0 +1,25 @@
+package basicsofjava;
+//Assignment25
+public class Print_evennumbers_upto100 {
+public static void main(String[] args) {
+	int no=100;
+	for(int i=0;i<=100;i++)
+	if(i%2==0)
+		System.out.println("it is even number  "+i);
+	
+	int no1=100;
+	for (int i=1;i<=100;i++)
+		if(i%1==1)
+			System.out.println("it is odd number  "+i);
+		
+	
+	
+
+}
+
+}
+
+	
+
+
+
