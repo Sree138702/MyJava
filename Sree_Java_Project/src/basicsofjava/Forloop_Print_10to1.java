0262f3fa3524b37ec1f7022543250ab438c63bd2
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-05 15:42:10 +0100

    Assignment 21

---
+++ Sree_Java_Project/src/basicsofjava/Forloop_Print_10to1.java
@@ -0,0 +1,12 @@
+package basicsofjava;
+//Assignment 21
+public class Forloop_Print_10to1 {
+	
+	public static void main(String[] args) {
+		
+		int i;
+		for(i=10;i>=1;i--)
+		System.out.println(i);
+
+		
+}}
