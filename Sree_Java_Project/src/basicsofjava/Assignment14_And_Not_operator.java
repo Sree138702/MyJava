14852513fb7763e4c9aaf6eb2a8a9936fc42dd37
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-05 11:21:26 +0100

    Assignment14

---
+++ Sree_Java_Project/src/basicsofjava/Assignment14_And_Not_operator.java
@@ -0,0 +1,21 @@
+package basicsofjava;
+
+public class Assignment14_And_Not_operator {
+	public static void main(String[] args) {
+		int age=30;
+		int salary=70000;
+		
+		if
+		(!(age>28 && salary<=80000))
+		{
+		System.out.println("person can buy a house");
+		
+		}
+		else
+		{
+			System.out.println("person can not buy a house");
+		}
+		
+	}
+
+}
