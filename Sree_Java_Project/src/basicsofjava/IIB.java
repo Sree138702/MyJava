1572df27ec141b5d478fa15f1d188f5a498db174
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-05 15:39:31 +0100

    Assignment 20

---
+++ Sree_Java_Project/src/basicsofjava/IIB.java
@@ -0,0 +1,19 @@
+package basicsofjava;
+public class IIB //Assignment20
+{
+	{
+		System.out.println("IIB 1");
+	}
+	{
+		System.out.println("IIB 2");
+	}
+	{
+		System.out.println("IIB 3");
+	}
+public static void main(String[] args) 
+{
+	new IIB();
+	new IIB();
+
+}
+}
