a84918e9b6f950b1d671cebfaab2a14160ea956e
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-05 15:45:10 +0100

    Assignment 22.1

---
+++ Sree_Java_Project/src/basicsofjava/Area_Of_A_Circle.java
@@ -0,0 +1,18 @@
+package basicsofjava;
+//Assignment 22(Circle)
+
+public class Area_Of_A_Circle {
+final static double pi=3.14;
+public static void main (String[]args) {
+	
+	int r=657;
+	double Areaofcircle = pi*r*r;
+	System.out.println(Areaofcircle);
+	
+	double Circumference_of_circle=2*pi*r;
+	System.out.println(Circumference_of_circle);
+}
+
+
+
+}
