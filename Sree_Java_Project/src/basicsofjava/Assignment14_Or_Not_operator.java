f82b697d295c2caf5b07c724e4aee4b212fb4461
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-05 15:14:51 +0100

    Assignment14

---
+++ Sree_Java_Project/src/basicsofjava/Assignment14_Or_Not_operator.java
@@ -0,0 +1,21 @@
+package basicsofjava;
+
+public class Assignment14_Or_Not_operator {
+	public static void main(String[] args) {
+		
+	
+	int age=20;
+	int salary=30000;
+	
+	if(!(age <= 18 || salary != 25000))
+	{
+	System.out.println("if condition is true");	
+	
+	}
+		
+	else
+	{
+		System.out.println("else condition should be executed");
+	}
+
+}}
