e500495d8b86e066b9bb330f7f6fa7667c91fc70
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-05 22:59:20 +0100

    Assignment 25.2

---
+++ Sree_Java_Project/src/basicsofjava/Print_Oddnumbers_upto100.java
@@ -0,0 +1,14 @@
+package basicsofjava;
+
+public class Print_Oddnumbers_upto100 {
+
+	public static void main(String[] args) {
+		int no=100;
+		for (int i=0;i<=100;i++)
+			if(i%2!=0)
+				System.out.println("it is odd number  "+i);
+			
+		
+	}
+
+}
