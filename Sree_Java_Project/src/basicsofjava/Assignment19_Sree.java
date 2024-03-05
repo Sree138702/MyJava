9866d7e1a9d7815b64f0baaf6f23b6c83597e38f
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-05 15:20:54 +0100

    Assignment19

---
+++ Sree_Java_Project/src/basicsofjava/Assignment19_Sree.java
@@ -0,0 +1,84 @@
+package basicsofjava;
+
+public class Assignment19_Sree {
+	
+	
+	public Assignment19_Sree()
+	{
+		System.out.println("constructor1");
+	}
+	
+	public Assignment19_Sree(int a) {
+		System.out.println("constructor2");
+		
+	}
+		
+	public Assignment19_Sree(int a,String b) {
+		System.out.println("constructor3");
+		
+	}
+	public Assignment19_Sree(int a,String b,float c) {
+		System.out.println("constructor4");
+		
+	}
+	public Assignment19_Sree(String b,float c,double d) {
+		System.out.println("constructor5");
+		
+	}
+	public Assignment19_Sree(int a,String b,char g) {
+		System.out.println("constructor6");
+	
+	}
+	public Assignment19_Sree(int a,String b,float c,boolean f) {
+		System.out.println("constructor7");
+	
+	}
+	
+	public Assignment19_Sree(int a,String b,boolean f) {
+		System.out.println("constructor8");
+	
+	}	
+	public Assignment19_Sree(int a,boolean f) {
+		System.out.println("constructor9");
+	
+	}	
+	public Assignment19_Sree(int a,double b) {
+		System.out.println("constructor10");
+	}
+	
+	public static void main(String[] args) {
+		
+	 Assignment19_Sree s1=new Assignment19_Sree();
+	 new Assignment19_Sree(1327);
+	 new Assignment19_Sree(250,"you");
+	 new Assignment19_Sree(3500,"me",23.65f);
+	 new Assignment19_Sree("we",77.68f,23.65);
+	 new Assignment19_Sree(6570,"they",'R');
+	 new Assignment19_Sree(750,"he",56.87f,true);
+	 new Assignment19_Sree(9500,"she",false);
+	 new Assignment19_Sree(87067,true);
+	 new Assignment19_Sree(67834,65.65);
+	 
+		 }
+
+	
+
+		
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
