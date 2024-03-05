3f3a4f387cc07fd75a35cb773446e223a1d22828
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-05 22:17:58 +0100

    Assignment 22.4

---
+++ Sree_Java_Project/src/basicsofjava/Area_Of_Triangle.java
@@ -0,0 +1,17 @@
+package basicsofjava;
+
+public class Area_Of_Triangle {
+ static int b=6;
+ static int h=8;
+ static int a=4;
+ static int c=12;
+ 
+ public static void main(String[] args) {
+	 int Area_of_triangle= h*b/2;
+	System.out.println(Area_of_triangle);
+	
+	
+	int Circumference_of_Triangle =a+b+c;
+	System.out.println(Circumference_of_Triangle);
+}
+}
