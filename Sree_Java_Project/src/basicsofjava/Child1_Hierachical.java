ced6a4d1a1d1c5158abd9c63441a85f36ce2583a
Sreeshubha <sreeshubha.c@gmail.com>
2024-03-07 00:01:23 +0100

    Assignment 33

---
+++ Sree_Java_Project/src/basicsofjava/Child1_Hierachical.java
@@ -0,0 +1,12 @@
+package basicsofjava;
+//Assignment33
+public class Child1_Hierachical extends Parent_Hierachical  {
+	
+public static void main(String[] args){
+	add();
+	Parent_Hierachical p1=new Parent_Hierachical ();
+	p1.sub();
+	
+	
+}
+}
